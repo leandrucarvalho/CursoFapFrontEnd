08fbf2d7fdb6b4ab97934cf34dc199dea12e6499
Leandro Carvalho <leandrucs@gmail.com>
2024-07-03 11:23:53 -0300

    update aula 2

---
+++ aula-02/array.ts
@@ -0,0 +1,7 @@
+// Questão 1
+
+let arr = [];
+
+// Questão 2
+
+arr.push(1, 2, 3);
---
+++ aula-02/outrosTiposVariaveis.ts
@@ -0,0 +1,25 @@
+// Questão 1
+
+let variavelNull = null;
+
+// Questão 2
+
+let variavelUndefined;
+
+// Questão 3
+
+console.log(variavelNull === null);
+
+// Questão 4
+
+console.log(variavelUndefined === undefined);
+
+// Questão 5
+
+const pessoa = {
+  name: "",
+};
+
+pessoa.name = "Leandro";
+
+console.log(pessoa);
