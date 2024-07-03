ba3ff24e956f5c38c7b6e3d6bc41255634d07c62
Leandro Carvalho <leandrucs@gmail.com>
2024-07-03 11:22:08 -0300

    add aula 3

---
+++ aula-03/index.js
@@ -0,0 +1,8 @@
+// const num1 = Number(prompt("Digite o primeiro número"));
+// const num2 = Number(prompt("Digite o segundo número"));
+// const resultado = num1 + num2;
+// console.log(resultado);
+var altura = Number(prompt("Digite sua altura:"));
+var peso = Number(prompt("Digite o seu peso:"));
+var imc = peso / (altura * altura);
+console.log(imc);
---
+++ aula-03/index.ts
@@ -0,0 +1,11 @@
+// const num1 = Number(prompt("Digite o primeiro número"));
+// const num2 = Number(prompt("Digite o segundo número"));
+// const resultado = num1 + num2;
+// console.log(resultado);
+
+const altura = Number(prompt("Digite sua altura:"));
+const peso = Number(prompt("Digite o seu peso:"));
+
+const imc = peso / (altura * altura);
+
+console.log(imc);
