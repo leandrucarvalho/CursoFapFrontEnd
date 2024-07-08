966c7dd66f7ceeee64fb24ab40812f704df683a4
Leandro Carvalho <leandrucs@gmail.com>
2024-07-08 11:03:42 -0300

    add aula 04

---
+++ aula-04/index.js
@@ -0,0 +1,30 @@
+// Questão 05
+// const nomePerson = prompt("Digite seu nome:");
+// alert(nomePerson);
+// Questão 06
+// const idadePerson2 = prompt("Digite sua idade");
+// let idadeConvertida2 = Number(idadePerson2);
+// alert(idadeConvertida2);
+// Questão 7
+// const numInt = prompt("Digite um número inteiro");
+// let result = parseFloat(numInt);
+// alert(result);
+// Questão 8
+// const num1 = Number(prompt("Digite um número inteiro: "));
+// const num2 = Number(prompt("Digite o segundo número: "));
+// const resultSoma = num1 + num2;
+// alert(resultSoma);
+// Questão 9
+// const numDecimal = parseFloat(prompt("digite um número decimal"));
+// const resultDecimal = numDecimal ** 2;
+// alert(resultDecimal.toFixed(2));
+// Questão 10
+// const anoNasc = Number(prompt("Digite o ano do seu nascimento: "));
+// let anoAtual = new Date();
+// let idade = anoAtual.getFullYear() - anoNasc;
+// alert(idade);
+// Questão 11
+// const primeiroNome = prompt("Digite seu primeiro nome: ");
+// const segundoNome = prompt("Digite seu segundo nome: ");
+// alert(primeiroNome + " " + segundoNome);
+// Questão 12 Solicite ao usuário uma sequência de números separados por espaço e exiba quantos números foram digitados.
